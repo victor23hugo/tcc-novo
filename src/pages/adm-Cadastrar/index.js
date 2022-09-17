da27ab2ccfe50ae73622773dacd965166ed585e6
victor23hugo <vh23032005@gmail.com>
2022-09-17 20:04:35 -0300

    teste3

--- src/pages/adm-Cadastrar/index.js
+++ src/pages/adm-Cadastrar/index.js
@@ -5,7 +5,7 @@ export default function Index(){
     return(
         <main className='page page-adm-cadastrar'>
             <div className=''>
-                <h1>Cadastrar Pedido</h1>
+                <h1>Cadastrardsadsa</h1>
             </div>
         </main>
     )
