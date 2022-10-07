8c5a7278492c61bf52690892bcbfe5635cf5c93f
victor23hugo <vh23032005@gmail.com>
2022-10-07 15:33:09 -0300

    filtrar camisa

--- site/src/pages/consultar/index.js
+++ site/src/pages/consultar/index.js
@@ -4,10 +4,57 @@ import Cabecalho from '../../componentes/cabecalho'
 
 export default function Index() {
     return(
-        <main className='page page-consultar'>
-            <Menu/>
+        <main className='page page-consultar'>              
+           
+            <Cabecalho />
             <div className='container'>    
-                <Cabecalho />
+                <Menu/>
+                <div className='listar'>
+                    <h1 className='produtos'>Produtos</h1>
+                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOME</th>
+                                <th>MARCA</th>
+                                <th>TAMANHO</th>
+                                <th>PREÇO</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td>São Paulo</td>
+                                <td>ADIDAS</td>
+                                <td>G</td>
+                                <td>200</td>
+                                <td><span>Editar</span></td>
+                                <td><span>Remover</span></td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>Santos</td>
+                                <td>UMBRO</td>
+                                <td>G</td>
+                                <td>300</td>
+                                <td><span>Editar</span></td>
+                                <td><span>Remover</span></td>
+                            </tr>
+                            <tr>
+                                <td>03</td>
+                                <td>Plameiras</td>
+                                <td>PUMA</td>
+                                <td>P</td>
+                                <td>199</td>
+                                <td><span>Editar</span></td>
+                                <td><span>Remover</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </main>
