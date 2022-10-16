68950a2f978fc41a1cf7b8427a8bb1db92e320ee
victor23hugo <vh23032005@gmail.com>
2022-10-16 14:53:44 -0300

    loginCliente quase 100%

---
+++ site/src/api/loginCliente.js
@@ -0,0 +1,11 @@
+import { API_URL } from './config';
+
+import axios from 'axios'
+const api = axios.create({
+    baseURL: API_URL
+})
+
+export async function logar(email, senha) {
+    const r = await api.post('/login', {email, senha });
+    return r.data;
+}
